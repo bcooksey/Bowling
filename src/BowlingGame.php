f4ff572a8fbee3190ffc4f7b9b8f8167927b0d1d
Brian Cooksey <bac2k@hotmail.com>
2012-05-29 21:15:27 -0500

    Refactor Scoring alg a bit

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -15,8 +15,7 @@ class BowlingGame {
         $this->current_frame->addRoll($score);
 	}
 
-
-	public function getScore(){
+    public function getScore() {
         // Scoring Rules:
         //   Open Frame - Total Pins
         //   Spare - 10 + next roll
@@ -25,26 +24,10 @@ class BowlingGame {
         $score = 0;
         for ($i = 0; $i < count($this->frames); $i++) {
             if ($this->frames[$i]->isStrike()) {
-                if (isset($this->frames[$i+1])) {
-                    $next_frame = $this->frames[$i+1];
-                    if ($next_frame->isStrike()) {
-                        if (isset($this->frames[$i+2])) {
-                            $score += array_pop($this->frames[$i+2]->getRolls());
-                        }
-                        $score += 10; // Next strike
-                    }
-                    else {
-                        $score += array_sum($next_frame->getRolls());
-                    }
-                }
-
-                $score += 10; // Current Strike
+                $score += $this->scoreStrike($i);
             }
             elseif ($this->frames[$i]->isSpare()) {
-                if (isset($this->frames[$i+1])) {
-                    $score += array_pop($this->frames[$i+1]->getRolls());
-                }
-                $score += 10; // Current Spare
+                $score += $this->scoreSpare($i);
             }
             else {
                 $score += array_sum($this->frames[$i]->getRolls());
@@ -52,4 +35,33 @@ class BowlingGame {
         }
         return $score;
 	}
+
+    protected function scoreStrike($i) {
+        $frame_score = 0;
+        if (isset($this->frames[$i+1])) {
+            $next_frame = $this->frames[$i+1];
+            if ($next_frame->isStrike()) {
+                if (isset($this->frames[$i+2])) {
+                    $frame_score += array_pop($this->frames[$i+2]->getRolls());
+                }
+                $frame_score += 10; // Next strike
+            }
+            else {
+                $frame_score += array_sum($next_frame->getRolls());
+            }
+        }
+
+        $frame_score += 10; // Current Strike
+        return $frame_score;
+    }
+
+    protected function scoreSpare() {
+        $frame_score = 0;
+        if (isset($this->frames[$i+1])) {
+            $frame_score += array_pop($this->frames[$i+1]->getRolls());
+        }
+
+        $frame_score += 10; // Current Spare
+        return $frame_score;
+    }
 }
